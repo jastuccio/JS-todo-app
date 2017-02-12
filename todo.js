05b32b40209b2c1a4883e31f162b7effcdd2788f
joe <joe@astucc.io>
2017-02-12 15:31:26 -0500

    Add v3 requirements

--- todo.js
+++ todo.js
@@ -1,10 +1,12 @@
 /*
-  V2 Requirements
-    completed:
-		  It should have a function to display todos
-		  It should have a function to add todos
-		  It should have a function to change todos
-		  It should have a function do delete todos
+  V3 Requirements
+    
+		  It should store the todos array on an object
+		  It should have a displayTodos method
+		  It should have an addTodo method
+		  It should have a deleteTodo method
+
+		  completed:
 		  
 */
 var todos = ["item 1", "item 2", "item 3"];
