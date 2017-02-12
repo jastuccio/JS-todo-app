4cac2d915b110a1a803a3693f9826da7d7f7e508
joe <joe@astucc.io>
2017-02-12 16:57:28 -0500

    Add the addTodo()

--- todo.js
+++ todo.js
@@ -1,12 +1,12 @@
 /*
   V3 Requirements
     
-		  It should store the todos array on an object
-		  It should have a displayTodos method
-		  It should have an addTodo method
 		  It should have a deleteTodo method
 
 		  completed:
+		    It should store the todos array on an object
+			  It should have a displayTodos method
+			  It should have an addTodo method
 		  
 */
 var todoList = {
@@ -14,15 +14,12 @@ var todoList = {
 
 	displayTodos: function() {
     console.log('My todos: ', this.todos);
+  },
+  addTodo: function(todo) {
+  	this.todos.push(todo);
+  	this.displayTodos();
   }
 };
-
-
-
-// function addTodo(str) {
-// 	todoList.push(str);
-// 	displaytodoList();
-// }
 
 // function changeTodo(position, newValue) {
 // 	todoList[position] = newValue;
