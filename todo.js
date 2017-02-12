a020d914fde1f0fbd3785d60cd7d13836d1d3c44
joe <joe@astucc.io>
2017-02-12 13:02:31 -0500

    Add displayTodos()

--- todo.js
+++ todo.js
@@ -1,1 +1,19 @@
-console.log('Todo World');
+/*
+  V2 Requirements
+    completed:
+		  It should have a function to display todos
+
+		required:  
+		  It should have a function to add todos
+		  It should have a function to change todos
+		  It should have a function do delete todos
+*/
+var todos = ["item 1", "item 2", "item 3"];
+
+function displayTodos() {
+  console.log('My todos:', todos);
+}
+
+function addTodo(str) {
+	
+}
