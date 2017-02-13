962e2acedc022909396988292b8f79c28d621958
joe <joe@astucc.io>
2017-02-12 20:40:49 -0500

    Add todoList.toggleCompleted

--- todo.js
+++ todo.js
@@ -1,33 +1,35 @@
 /*
   V4 Requirements
-    
-		  todoList.changeTodo should change the todoText property
-		  todoList.toggleCompleted should changethe completed property
-
-		  completed:
-		     todoList.addTodo should add objects
-		     todoList.changeTodo should change the todoText property  
+    completed:
+		  todoList.addTodo should add objects
+      todoList.changeTodo should change the todoText property 
+      todoList.toggleCompleted should changethe completed property
 */
 var todoList = {
 	todos: [],
 
 	displayTodos: function() {
-    console.log('My todos: ', this.todos);
-  },
-  addTodo: function(todoText) {
-  	this.todos.push({
-      todoText: todoText,
-      completed: false
-  	}),
-  	this.displayTodos();
-  },
-  changeTodo: function(position, todoText) {
-	  this.todos[position].todoText = todoText;
-	  this.displayTodos();
-  },
-  deleteTodo: function(position) {
-  	this. todos.splice(position, 1);
-  	this.displayTodos();
-  }
+		console.log('My todos: ', this.todos);
+	},
+	addTodo: function(todoText) {
+		this.todos.push({
+				todoText: todoText,
+				completed: false
+			}),
+			this.displayTodos();
+	},
+	changeTodo: function(position, todoText) {
+		this.todos[position].todoText = todoText;
+		this.displayTodos();
+	},
+	deleteTodo: function(position) {
+		this.todos.splice(position, 1);
+		this.displayTodos();
+	},
+	toggleCompleted: function(position) {
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		this.displayTodos();
+	}
 
 };
