3667a0ae7b2fdc45ca9b1274b611f0e4703ea0ce
joe <joe@astucc.io>
2017-02-15 12:03:59 -0500

    .displayTodos indaicates complete or incomplete for each todo

--- todo.js
+++ todo.js
@@ -1,10 +1,10 @@
 /*
   V5 Requirements
-    .displayTodos should show .completed
 
     completed:
       .displayTodos should show .todoText
       .displayTodos should tell you if .todos is empty
+      .displayTodos should show .completed
 */
 var todoList = {
 	todos: [],
@@ -13,9 +13,12 @@ var todoList = {
 		if (this.todos.length == 0) {
 	    console.log('Your todo list is empty!');
     } else {
-			console.log('My todos: ');
 			for (var i = 0; i < this.todos.length; i++) {
-				console.log(this.todos[i].todoText);
+				if (this.todos[i].completed === true) {
+			  console.log('(x) ', this.todos[i].todoText);
+			  } else {
+			 	  console.log('( ) ', this.todos[i].todoText);
+			  }
 			}
 		}
 	},
